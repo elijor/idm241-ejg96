dd95dd7363d113658c2db14a609b9f507c64dfbe
elijor <ejg96@drexel.edu>
2023-12-11 23:14:03 -0500

    add casestudy plus fix

--- idm241/final/build/script.js
+++ idm241/final/build/script.js
@@ -76,10 +76,22 @@ function openModal3() {
 function closeModal() {
     console.log("testing-close");
     mBackground.classList.add("hiddenmodal-b");
-    modal.classList.add("hiddenmodal");
+    // modal.classList.add("hiddenmodal");
     console.log("testing-close-before-reset");
 
     resetCounter();
+    var sbutton1 = document.getElementById('b1');
+    var sbutton2 = document.getElementById('b2');
+    var sbutton3 = document.getElementById('b3');
+    var sbutton4 = document.getElementById('b4');
+    var sbutton5 = document.getElementById('b5');
+
+
+    sbutton1.classList.remove('selected');
+    sbutton2.classList.remove('selected');
+    sbutton3.classList.remove('selected');
+    sbutton4.classList.remove('selected');
+    sbutton5.classList.remove('selected');
     console.log("testing-close-after-reset");
 };
 
@@ -91,14 +103,26 @@ function addedtocart() {
     mBackground.classList.add("hiddenmodal-b");
     textchange.innerText = "Add to Cart";
     textchange.classList.remove("bold");
+    var sbutton1 = document.getElementById('b1');
+    var sbutton2 = document.getElementById('b2');
+    var sbutton3 = document.getElementById('b3');
+    var sbutton4 = document.getElementById('b4');
+    var sbutton5 = document.getElementById('b5');
+
+
+    sbutton1.classList.remove('selected');
+    sbutton2.classList.remove('selected');
+    sbutton3.classList.remove('selected');
+    sbutton4.classList.remove('selected');
+    sbutton5.classList.remove('selected');
   }, 900);
 };
 
 
-function heartLike() {
-  const heart = document.querySelector(".favbutton");
-  heart.classList.add('active');
-};
+// function heartLike() {
+//   const heart = document.querySelector(".unliked");
+//   heart.classList.add('active');
+// };
 
 document.addEventListener('DOMContentLoaded', function () {
   // Get references to the buttons
@@ -167,37 +191,138 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 });
 
+document.addEventListener('DOMContentLoaded', function () {
+  // Select the like button
+  const likeButton = document.getElementById('likeButton');
 
-var likedElements = document.querySelectorAll('.liked:hover');
-
-likedElements.forEach(function(likedele) {
-  likedele.addEventListener('click', function() {
-    // Add the 'shake' class for horizontal shake animation
-    this.classList.add('shake');
-
-    setTimeout(() => {
-      this.classList.remove('shake');
-      this.classList.add('unliked');
-      this.classList.remove('liked');
-    }, 400);
+  // Add click event listener to the like button
+  likeButton.addEventListener('click', function () {
+    // Check if the button has the 'unliked' class
+    if (likeButton.classList.contains('unliked')) {
+      likeButton.classList.add('bounce');
+      setTimeout(() => {
+        likeButton.classList.remove('bounce');
+        likeButton.classList.add('liked');
+        likeButton.classList.remove('unliked');
+      }, 200);
+    } else {
+      likeButton.classList.toggle('shake');
+      setTimeout(() => {
+        likeButton.classList.remove('liked');
+        likeButton.classList.remove('shake');
+        likeButton.classList.add('unliked');
+      }, 200);
+    }
   });
 });
 
-var heartElements = document.querySelectorAll('.unliked');
+document.addEventListener('DOMContentLoaded', function () {
+  // Select the like button
+  const likeButton = document.getElementById('likeButton2');
 
-heartElements.forEach(function(heart) {
-  heart.addEventListener('click', function() {
-    // Toggle the bounce class
-    this.classList.toggle('bounce');
-
-    setTimeout(() => {
-
-      this.classList.remove('bounce');
-      this.classList.remove('unliked');
-      this.classList.add('liked');
-    }, 300);
+  // Add click event listener to the like button
+  likeButton.addEventListener('click', function () {
+    // Check if the button has the 'unliked' class
+    if (likeButton.classList.contains('unliked')) {
+      likeButton.classList.add('bounce');
+      setTimeout(() => {
+        likeButton.classList.remove('bounce');
+        likeButton.classList.add('liked');
+        likeButton.classList.remove('unliked');
+      }, 200);
+    } else {
+      likeButton.classList.toggle('shake');
+      setTimeout(() => {
+        likeButton.classList.remove('liked');
+        likeButton.classList.remove('shake');
+        likeButton.classList.add('unliked');
+      }, 200);
+    }
   });
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+  // Select the like button
+  const likeButton = document.getElementById('likeButton3');
+
+  // Add click event listener to the like button
+  likeButton.addEventListener('click', function () {
+    // Check if the button has the 'unliked' class
+    if (likeButton.classList.contains('unliked')) {
+      likeButton.classList.add('bounce');
+      setTimeout(() => {
+        likeButton.classList.remove('bounce');
+        likeButton.classList.add('liked');
+        likeButton.classList.remove('unliked');
+      }, 200);
+    } else {
+      likeButton.classList.toggle('shake');
+      setTimeout(() => {
+        likeButton.classList.remove('liked');
+        likeButton.classList.remove('shake');
+        likeButton.classList.add('unliked');
+      }, 200);
+    }
+  });
+});
+
+
+// let heartElements = document.querySelectorAll('.unliked');
+
+// heartElements.forEach(function(heart) {
+//   heart.addEventListener('click', function() {
+//     // Toggle the bounce class
+//     this.classList.toggle('bounce');
+
+//     setTimeout(() => {
+
+//       this.classList.remove('bounce');
+//       this.classList.remove('unliked');
+//       this.classList.add('liked');
+//     }, 300);
+//   });
+// });
+
+
+
+
+// let likedElements = document.querySelectorAll('.liked');
+
+// likedElements.forEach(function(likedele) {
+//   likedele.addEventListener('click', function() {
+//     // Add the 'shake' class for horizontal shake animation
+//     this.classList.add('shake');
+
+//     setTimeout(() => {
+//       this.classList.remove('shake');
+//       this.classList.add('unliked');
+//       this.classList.remove('liked');
+//     }, 400);
+//   });
+// });
+
+
+// // document.addEventListener('DOMContentLoaded', function () {
+// //   // Select the like button
+//   const likeButton = document.getElementById('favorite1');
+
+//   // Add click event listener to the like button
+//   likeButton.addEventListener('click', function () {
+//     // Check if the button has the 'unliked' class
+//     if (likeButton.classList.contains('unliked')) {
+//       // Remove 'unliked', add 'liked'
+//       likeButton.classList.remove('unliked');
+//       likeButton.classList.add('liked');
+//       likeButton.textContent = 'Unlike';
+//     } else {
+//       // Remove 'liked', add 'unliked'
+//       likeButton.classList.remove('liked');
+//       likeButton.classList.add('unliked');
+//       likeButton.textContent = 'Like';
+//     }
+//   });
+// });
+
 
 
 
